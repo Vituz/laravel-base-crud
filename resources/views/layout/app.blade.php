44b76fbcd11e8779e7b92c756c9a4aa8566808d3
Vituz <vtbattaglia@gmail.com>
2021-07-14 17:08:11 +0200

    Create partials header and footer

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+
+<body>
+    <header id="site_header">
+        @include('partials.header')
+    </header>
+
+    <main id="site_main">
+        @yield('content')
+    </main>
+
+    <footer id="site_footer"></footer>
+    @include('partials.footer')
+</body>
+
+</html>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,1 @@
+<h3>site footer</h3>
